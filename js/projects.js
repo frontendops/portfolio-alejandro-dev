0a4bd91708fa89c31881d390151a926f1153e8e6
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-03-23 20:45:19 -0400

    saved project data external

---
+++ js/projects.js
@@ -0,0 +1,58 @@
+export const projects = [
+   {
+      title: 'GardenU',
+      description: "Allowing gardeners, plant lovers, and students to create, update, and delete entire gardens with plant info. React/Redux front end with Node.js backend.",
+      technology: 'react',
+      link: 'https://evening-refuge-33971.herokuapp.com/',
+      img: 'images/garden-u.jpg',
+      button: 'Try Demo'
+   },
+   {
+      title: 'Green Tech upstate',
+      description: "Showing customers the benefits of solar energy in their homes. Allowing customers to get quotes on solar, roofing and HVAC. In collaboration with Green Tech upstate.",
+      technology: 'wordpress',
+      link: 'https://www.greentechupstate.com',
+      img: 'images/garden-u.jpg',
+      button: 'view site'
+   },
+   {
+      title: 'Quizzly',
+      description: "App for students to create quizzes and study.",
+      technology: 'react',
+      link: 'https://quizzly-io.herokuapp.com',
+      img: 'images/library.jpeg',
+      button: 'Try Demo'
+   },
+   {
+      title: 'Matching game',
+      description: "Matching card game built with vanilla JavaScript.",
+      technology: 'javascript',
+      link: 'http://minimalmvnt.com/memory%20game%20v2/',
+      img: 'images/memory_photos.jpg',
+      button: 'Try Demo'
+   },
+   {
+      title: 'Neighborhood Map',
+      description: "Built with react js, this is a map of my city and some of the places i like to visit.",
+      technology: 'react',
+      link: 'https://github.com/frontendops/My-neighborhood-map',
+      img: 'images/map_imageW.jpg',
+      button: 'Learn more'
+   },
+   {
+      title: 'WordPress site Salon',
+      description: "Site for local hair salon built with wordpress.",
+      technology: 'wordpress',
+      link: 'http://www.salonexclusivo.com',
+      img: 'images/salon_outside.jpg',
+      button: 'view site'
+   },
+   {
+      title: 'Wordpress site blog',
+      description: "Blog for anime channel built with wordpress",
+      technology: 'wordpress',
+      link: 'http://www.easternenvoy.com',
+      img: 'images/anime_feels.jpg',
+      button: 'view site'
+   },
+]
