78a7ba1c63dc1a26b4088389657e9c3050cce576
Alejandro-Sierra <alexsierra1598@gmail.com>
2019-03-25 10:42:48 -0400

    added new project to vue section

--- js/app.js
+++ js/app.js
@@ -1,3 +1,38 @@
 import { projects } from './projects.js';
 
-console.log(projects);
+
+
+//buttons to filter projects
+
+const buttons = document.querySelector('.btn-container');
+
+const projectsBody = document.querySelector(".features");
+
+projects.forEach(project => {
+   projectsBody.innerHTML += `<article> <a href="${project.link}" class="image"><img src="${project.img}" alt="${project.alt}" /></a> <h3 class="major">${project.title}</h3> <p>${project.description}</p> <a href="${project.link}" class="special">${project.button}</a> </article>`
+})
+
+function renderNewprojects(project) {
+   projectsBody.innerHTML += `<article> <a href="${project.link}" class="image"><img src="${project.img}" alt="${project.alt}" /></a> <h3 class="major">${project.title}</h3> <p>${project.description}</p> <a href="${project.link}" class="special">${project.button}</a> </article>`
+}
+
+
+
+buttons.addEventListener('click', (e) => {
+   const primary = document.querySelector('.primary');
+   if (primary) {
+      primary.classList.remove('primary')
+   }
+   e.target.classList.add('primary')
+
+   projectsBody.innerHTML = '';
+   projects.forEach(project => {
+      if (project.technology === e.target.innerHTML.toLowerCase()) {
+
+         renderNewprojects(project);
+      } else if (e.target.innerHTML.toLowerCase() === 'all') {
+         renderNewprojects(project)
+      }
+   })
+
+})
--- js/projects.js
+++ js/projects.js
@@ -1,10 +1,20 @@
 export const projects = [
    {
+      title: 'Productivity Manager',
+      description: "Keep track of daily taks and see how much you have accomplished in a day.",
+      technology: 'vue',
+      link: 'http://www.easternenvoy.com',
+      img: 'images/tasks.jpg',
+      alt: 'computer productivity',
+      button: 'view site'
+   },
+   {
       title: 'GardenU',
       description: "Allowing gardeners, plant lovers, and students to create, update, and delete entire gardens with plant info. React/Redux front end with Node.js backend.",
       technology: 'react',
       link: 'https://evening-refuge-33971.herokuapp.com/',
       img: 'images/garden-u.jpg',
+      alt: 'Orange tulip',
       button: 'Try Demo'
    },
    {
@@ -12,7 +22,8 @@ export const projects = [
       description: "Showing customers the benefits of solar energy in their homes. Allowing customers to get quotes on solar, roofing and HVAC. In collaboration with Green Tech upstate.",
       technology: 'wordpress',
       link: 'https://www.greentechupstate.com',
-      img: 'images/garden-u.jpg',
+      img: 'images/foggy_forest.jpg',
+      alt: 'foggy rain forest',
       button: 'view site'
    },
    {
@@ -21,6 +32,7 @@ export const projects = [
       technology: 'react',
       link: 'https://quizzly-io.herokuapp.com',
       img: 'images/library.jpeg',
+      alt: 'beautiful library',
       button: 'Try Demo'
    },
    {
@@ -29,6 +41,7 @@ export const projects = [
       technology: 'javascript',
       link: 'http://minimalmvnt.com/memory%20game%20v2/',
       img: 'images/memory_photos.jpg',
+      alt: 'man holding photos',
       button: 'Try Demo'
    },
    {
@@ -37,6 +50,7 @@ export const projects = [
       technology: 'react',
       link: 'https://github.com/frontendops/My-neighborhood-map',
       img: 'images/map_imageW.jpg',
+      alt: 'maps on desk',
       button: 'Learn more'
    },
    {
@@ -45,6 +59,7 @@ export const projects = [
       technology: 'wordpress',
       link: 'http://www.salonexclusivo.com',
       img: 'images/salon_outside.jpg',
+      alt: 'outside of shops',
       button: 'view site'
    },
    {
@@ -53,6 +68,8 @@ export const projects = [
       technology: 'wordpress',
       link: 'http://www.easternenvoy.com',
       img: 'images/anime_feels.jpg',
+      alt: 'tokyo night roads',
       button: 'view site'
    },
+
 ]
